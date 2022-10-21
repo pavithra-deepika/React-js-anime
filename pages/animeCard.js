9618c0db33efe9f969dbde1b940a4a71bd44c3e3
pavithra <pavithradeepika301@gmail.com>
2022-10-21 23:18:01 +0530

    drag-drop

--- pages/animeCard.js
+++ pages/animeCard.js
@@ -3,24 +3,61 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Grid } from '@mui/material';
-import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
 
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 250,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  media: {
+    height: '10', 
+    width:'100',
+  }
+};
 export default function AnimeCard({movie}) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>        
-        <CardContent>
-        <img src = {movie.images.jpg.image_url}></img>
-          <Typography gutterBottom variant="h5" component="div">
+    <div>
+    <Modal
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+    >
+  <Box sx={style} 
+    onClick={handleClose}>
+      <img src = {movie.images.jpg.image_url} ></img> 
+      <Typography gutterBottom variant="h5" component="div">
+          {movie.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Episodes: {movie.episodes} <br /> Rating: {movie.rating} <br /> duration: {movie.duration} <br /> Season: {movie.season} <br /> Year: {movie.year} <br /> Rank: {movie.rank} <br /> genres: {movie.genres.map(genre => genre.name + ", ",)}
+        </Typography>
+  </Box>
+</Modal>
+    <Card sx={{ maxWidth: 345 }} onClick={handleOpen}>
+      <CardActionArea >   
+        <CardContent>  
+        <img src = {movie.images.jpg.image_url} style={style.media}  ></img>
+          <Typography gutterBottom variant="h5" component="div"  sx={{ whiteSpace: 'nowrap' }}>
             {movie.title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-          Episodes: {movie.episodes}, genres: {movie.genres.map(genre => genre.name + ", ")}
+          <Typography variant="body2" color="text.secondary"  sx={{ whiteSpace: 'nowrap' }}>
+          Episodes: {movie.episodes} <br /> Rating: {movie.rating} <br /> genres: {movie.genres.map(genre => genre.name + ", ")}
           </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,71 @@
-import { Grid, MenuItem, Pagination, Select, TextField } from '@mui/material';
+import { Button, Grid, MenuItem, Pagination, Select, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import AnimeCard from './animeCard';
 import FormControl from '@mui/material/FormControl';
+
 export default function Home() {
   const [filters,setFilter] = useState([0]);
   const [pages,setPage] = useState();
   const [animes,setAnimes] = useState([]);
   const [genres, setGenre] = useState('');
   useEffect(() => {
-    
-    fetchAnimes();
+    fetchAnimes();  
     filterAnimes();
     },[]);
+
   let movies;
   const fetchAnimes = async () => {
     const data=await fetch("https://api.jikan.moe/v4/anime");
     movies=await data.json();
     setAnimes(movies.data);
     movies.pagination ? setPage(movies.pagination.last_visible_page) : "";
-    
     }; 
-    
+    // search
   const searchAnimes = async () => {
     let val= document.getElementById("searchAnime").value
     const data=await fetch("https://api.jikan.moe/v4/anime?q="+val);
     movies=await data.json();
     setAnimes(movies.data)
     }; 
+    // filter
   const filterAnimes = async () => {
-    const data=await fetch("https://api.c.moe/v4/genres/anime");
+    const data=await fetch("https://api.jikan.moe/v4/genres/anime");
     let filter=await data.json();
     setFilter(filter.data)
-    }; 
-      
-  const handleChange = async(event) => {
+    };  
+  const hanasyncdleChange = (event) => {
     setGenre(event.target.value);
     const data=await fetch("https://api.jikan.moe/v4/anime?genres="+event.target.value);
     movies=await data.json();
     setAnimes(movies.data)
     };
+    // pagination
   const changePage= async(event,value)=>{
     const data=await fetch("https://api.jikan.moe/v4/anime?page="+value);
     movies=await data.json();
     setAnimes(movies.data)
     }
+
+    function drag(e, ev) {
+      console.log('Drag Event', ev)
+      e.dataTransfer.setData("text", ev);
+    }
+
+    function drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      console.log(data)
+      // ev.target.appendChild(document.getElementById(data));
+    }
         
   return (
     <div>
-      <Grid container  sx={{ p:2}}>                 
-        <Grid item xs={6} >
-          <TextField id="searchAnime" label="Outlined" variant="outlined" placeholder ='search'  size="small"/>
-          <button onClick={searchAnimes}>search</button>
-          
-        </Grid>    
-        <Grid item xs={6}>
+      <h2>React JS Animes</h2>
+      <Grid container  sx={{ p:2}}> 
+        <Grid item xs={3}>
           {filters ?
-          <FormControl  sx={{ minWidth: 200 }}>
+          <FormControl  sx={{ minWidth: 150 }}>
           <InputLabel id="demo-simple-select-label">Anime Genre</InputLabel>
           <Select
           labelId="demo-simple-select-label"                
@@ -67,32 +76,37 @@ export default function Home() {
           >
             {filters.map(elem => (            
             <MenuItem  key={elem.mal_id} value={elem.mal_id}>{elem.name}</MenuItem>   
-            )) }          
-            
+            )) }   
           </Select>
           </FormControl> 
           : ""}
-               
+             
         </Grid>
+        <Grid item xs={5} >
+        <Pagination  sx={{ p:1}} count={pages} onChange={changePage}/> 
+        </Grid>
+        <Grid item xs={4} >
+          <TextField id="searchAnime" label="search" variant="outlined" placeholder ='search'  size="small"/>
+          <Button variant="outlined" sx={{ marginLeft:2}} onClick={searchAnimes}>search</Button>3
+        </Grid> 
       </Grid>
       <Grid container  sx={{ p:1}}>
         <Grid item xs={9}>
-          <Pagination  sx={{ p:1}} count={pages} onChange={changePage}/>
           <Grid
           container
-          spacing={2}
+          spacing={2} 
           direction="row"
           justify="flex-start"
           alignItems="flex-start"
           >
           {animes ?  animes.map(elem => (
-             <Grid item xs={12} sm={6} md={3}  draggable >
+             <Grid id={elem.id} item xs={12} sm={6} md={3} key={animes.indexOf(elem)} draggable  onDragStart={(e) => drag(e, elem.title)} >
                  <AnimeCard movie={elem}/> 
              </Grid>
           )) : "No Result Found"}
           </Grid>
         </Grid>
-        <Grid item xs={3}> Watchs</Grid>
+        <Grid item onDrop={(e) => drop(e)} onDragOver={(e)=> e.preventDefault()} xs={3}><h4>WatchList</h4> </Grid>
       </Grid>
     </div>
   );
